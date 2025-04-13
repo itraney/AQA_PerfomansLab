5b2ade62ff6573e8146f709aa37d2875a6bf3a99
Valentovich Yan <valentovican@gmail.com>
2025-04-13 22:59:48 +0300

    Вопрос по задаче 4 (при использовании функции Round округляется только по десятым, сотые и тысячные не учитываются, что немного смущает)...

---
+++ src/HomeWork_1/HomeTask_2.java
@@ -0,0 +1,13 @@
+package HomeWork_1;
+
+public class HomeTask_2 {
+
+    public static void main(String[] args) {
+
+        int n = 90;
+        int a = n/10;
+        int b = n%10;
+
+        System.out.println(a + b);
+    }
+}
