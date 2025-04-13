c58fe7f7aceddd8529fa8185c55fdd89adb72f31
Valentovich Yan <valentovican@gmail.com>
2025-04-13 11:43:11 +0300

    1 задача и ее разширенное решение

---
+++ src/HomeWork_1/HomeWork_1_1.java
@@ -0,0 +1,20 @@
+package HomeWork_1;
+
+import java.util.Scanner;
+
+public class HomeWork_1_1 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите значение b: ");
+        double b = scanner.nextDouble();
+
+        System.out.print("Введите значение с: ");
+        double c = scanner.nextDouble();
+
+    int a = (int) (4*(b+c-1)/2);
+
+    System.out.println(a);
+}
+}
